ca437f1bcfe6102f228de923ddf2b32c212158c9
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-08 06:36:50 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,9 +33,9 @@ end
 def hit?(number)
   prompt_user
   input = get_user_input
-  if input == 's' do
+  if input == 's'
     number
-  elsif input == 'h' do
+  elsif input == 'h'
     new_number = deal_card
     total = number + new_number
     total
