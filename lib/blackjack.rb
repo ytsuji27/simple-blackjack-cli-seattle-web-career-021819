80b65832daf16cefa84ccf633e2836de35ae8fad
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-08 06:47:49 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,7 +56,7 @@ end
 def runner
   welcome
   card total = initial_round
-  until card_total > 21 do
+  until card_total > 21
     card_total += hit?(card_total)
     display_card_total(card_total)
   end
